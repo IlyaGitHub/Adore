4da7d22e26cad3993582e31eb22468e53abc6bb4
IlyaGitHub <ilya-levoshko@mail.ru>
2021-12-04 23:10:25 +0300

    Admin panel made part 1

--- database/migrations/2021_12_02_172217_create_options_table.php
+++ database/migrations/2021_12_02_172217_create_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSettingsTable extends Migration
+class CreateOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSettingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('settings', function (Blueprint $table) {
+        Schema::create('options', function (Blueprint $table) {
             $table->id();
             $table->string('key');
             $table->text('value');
@@ -28,6 +28,6 @@ class CreateSettingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('settings');
+        Schema::dropIfExists('options');
     }
 }
